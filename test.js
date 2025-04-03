6a24dc04e596e06698e0d98225da5480630a71b6
Giorgi <giorgi.mchedlo.092@gmail.com>
2025-04-03 19:41:36 +0400

    changed

---
+++ test.js
@@ -0,0 +1,28 @@
+//let person = {
+//  FirstName: "dodo",
+//  LastName: "barbaqadze",
+//  Age:77,
+//  isAdult: true,
+//  address: {
+//  city: "Tbilisi",
+//  street: "tsereteli",
+//  Number: "12"
+// },
+//  prof:["Teseter","developer", "BA"]
+// }
+// console.log(person.address)
+// console.log(person.prof[0])
+
+
+// let a=7
+// let b= "8"
+// console.log(a===b)
+
+
+
+
+// tu<18 sesxs ar vadzlevt - vwert rom arasrulwlovania
+// tu 18-65-mde aris sesxs vadzlevt
+// tu 65> sesxs ar vadzlevt 
+
+
